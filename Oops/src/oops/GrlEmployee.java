3dd17ca3115a1420bd836fbf53f0cd32b42b1f4f
karthi <karthi@LAPTOP-MGT5DUAA>
2022-04-13 13:11:16 +0530

    second commit

---
+++ Oops/src/oops/GrlEmployee.java
@@ -0,0 +1,29 @@
+package oops;
+public class GrlEmployee 
+{
+    int empid;
+    String empname;
+    int empsalary;
+    String empdepartment;
+    int empexperience;
+    void display ()
+    {
+    	System.out.println(empid);
+    	System.out.println(empname);
+    	System.out.println(empsalary);
+    	System.out.println(empdepartment);
+    	System.out.println(empexperience);
+    }
+    public static void main(String args[])
+    {
+	  GrlEmployee emp1=new GrlEmployee();
+	   emp1.empid=110;
+	   emp1.empname="Balaji";
+	   emp1.empsalary=60000;
+	   emp1.empdepartment="DEV";
+	   emp1.empexperience=5;
+	   emp1.display();
+    }
+}
+    
+
