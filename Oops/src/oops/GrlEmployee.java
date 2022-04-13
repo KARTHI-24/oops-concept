2aff36c187448d3dce0fc075680f203db246a392
karthi <karthi@LAPTOP-MGT5DUAA>
2022-04-13 13:21:37 +0530

    grl

--- Oops/src/oops/GrlEmployee.java
+++ Oops/src/oops/GrlEmployee.java
@@ -20,7 +20,7 @@ public class GrlEmployee
 	   emp1.empid=110;
 	   emp1.empname="Balaji";
 	   emp1.empsalary=60000;
-	   emp1.empdepartment="DEV";
+	   emp1.empdepartment="DEVE";
 	   emp1.empexperience=5;
 	   emp1.display();
     }
