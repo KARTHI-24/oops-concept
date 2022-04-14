299b0df760aacaee646261905c8088cccb787d95
karthi <karthi@LAPTOP-MGT5DUAA>
2022-04-14 14:05:30 +0530

    fourthcommit

--- Oops/src/ControlStatements.java
+++ Oops/src/ControlStatements.java
@@ -44,7 +44,7 @@ public class ControlStatements {
 //		  while(i>0)
 //		  {
 //			  System.out.println(i);
-//			 i--;
+//			 i--;-------------->output 10,9,8,7,6,5,4,3,2,1
 //		 }
 		// do while
 //		 int i=20;
@@ -53,27 +53,26 @@ public class ControlStatements {
 //			{
 //				System.out.println(i);
 //				i++;
-//			}while(i<=30);
+//			}while(i<=30);---->output 20
 		 
 		 // for(int i=10;i>0;i--)using for to find decending order
 		// {
-		//	 System.out.println(i);		 }
+		//	 System.out.println(i);		 }output10,9,8,7,6,5,4,3,2,1
 		 
 	// }
 //	 for(int i=1;i<=10;i++) ascending order using for
 //	 {
-//		 System.out.println(i);		 }
+//		 System.out.println(i);		 }output 1,2,3,4,5,6,7,8,9,10
 //	 //jump statements (break;and continue;
 		 
 
 //		  for(int i=10;i>0;i--)
 //		{
 //			  if(i==5|| i==3|| i==1)continue;
-//			 System.out.println(i);		 }
-
-		  for(int i=10;i>0;i--)
-		{
-			  if(i==5)break;
-			 System.out.println(i);		 }
+//			 System.out.println(i);		 }output------->10,9,8,7,6,4,2 increment 2,4,6,7,8,9,10(int i=1;i<=10;i++)
+//		  for(int i=10;i>0;i--)
+//		{
+//			  if(i==5)break;
+//			 System.out.println(i);		 }output----->10,9,8,7,6 if icrement 1,2,3,4(int i=1;i<=10;i++)
  }
 }
