b7be4813deffc198c6a25e357d2adedc93e556d5
karthi <karthi@LAPTOP-MGT5DUAA>
2022-04-14 14:05:08 +0530

    controlstatements

---
+++ Oops/src/ControlStatements.java
@@ -0,0 +1,79 @@
+
+public class ControlStatements {
+	
+	 public static void main(String args[]) {
+		 
+//		 int i=20;//conditional statements
+//		 
+//		 if(i>=20)
+//		 {
+//			 
+//			 System.out.println("ELIGIBLE FOR VOTE");
+//		 }
+//		 
+//		 
+//		 else if(i==20)
+//		 {
+//			 System.out.println("ELIGIBLE");
+//		 }
+//		 
+//		 else if(i==30)
+//		 {
+//			 System.out.println("vote");
+//		 }
+		 
+			 //looping sum of even numbers using while
+//		 int i = 2;
+//		  
+//		  while(i<=10)
+//		  {
+//			  System.out.println(i);
+//			 i+=2;
+//		 }
+		 //sum of odd numbers
+//		 int i = 1;
+//		  
+//		  while(i<=10)
+//		  {
+//			  System.out.println(i);
+//			 i+=2;
+//		 }
+			 //decending numbers
+//		 int i = 10;
+//		  
+//		  while(i>0)
+//		  {
+//			  System.out.println(i);
+//			 i--;
+//		 }
+		// do while
+//		 int i=20;
+//				 
+//		   do
+//			{
+//				System.out.println(i);
+//				i++;
+//			}while(i<=30);
+		 
+		 // for(int i=10;i>0;i--)using for to find decending order
+		// {
+		//	 System.out.println(i);		 }
+		 
+	// }
+//	 for(int i=1;i<=10;i++) ascending order using for
+//	 {
+//		 System.out.println(i);		 }
+//	 //jump statements (break;and continue;
+		 
+
+//		  for(int i=10;i>0;i--)
+//		{
+//			  if(i==5|| i==3|| i==1)continue;
+//			 System.out.println(i);		 }
+
+		  for(int i=10;i>0;i--)
+		{
+			  if(i==5)break;
+			 System.out.println(i);		 }
+ }
+}
