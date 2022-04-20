fc1c055d31d359bbca6090c8a5b5675c0356c843
karthi <karthi@LAPTOP-MGT5DUAA>
2022-04-20 15:33:07 +0530

    STRING

---
+++ Oops/src/Stringb.java
@@ -0,0 +1,18 @@
+
+
+public class Stringb {
+
+	public static void main(String[] args) {
+		String s1 ="venkat";
+     System.out.println(s1);
+     char ch=s1.charAt(2);
+     System.out.println(ch);
+  //eauals
+     String s11= "venkat";
+     System.out.println(s11);
+     boolean b = s11.equals("venkat");
+     System.out.println(b);
+    		 
+	}
+
+}
