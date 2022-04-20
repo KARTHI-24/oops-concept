2a7597de0c98fdb9ddcf2bb6cacad08d3a5fd9bc
karthi <karthi@LAPTOP-MGT5DUAA>
2022-04-20 15:46:46 +0530

    COMMIT4

--- Oops/src/AbstractAnimal.java
+++ Oops/src/AbstractAnimal.java
@@ -1,4 +1,35 @@
 
-public class AbstractAnimal {
+abstract class AbstractAnimal {
+	// always implementation of abstract methods will be written in drived class
+	abstract  void  move();
+	abstract  void  eat();
+	
+	// concrete method or concrete class it contains complete implementation part
+	void  label()  {
+		
+	System.out.println("Animal's data:");
+	}
+	
+	}
 
+
+class  Bird  extends AbstractAnimal {
+void  move()  {
+	
+System.out.println("Moves by flying.");
+}
+void  eat()  {
+	
+System.out.println("Eats birdfood.");
+}
+
+}
+
+class  Fish  extends AbstractAnimal {
+void  move()  {
+System.out.println("Moves by swimming.");
+}
+void  eat()  {
+System.out.println("Eats seafood.");
+}
 }
